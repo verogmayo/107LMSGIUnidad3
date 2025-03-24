8d98bc8640fabf7ddba65aae33fb7a0a269400e9
Veronique_Grue <veronique.gru@educa.jcyl.com>
2025-03-24 09:21:31 +0100

    primer commit Ejercicio 1 -

--- 01/js/script.js
+++ 01/js/script.js
@@ -1,2 +1,19 @@
+var titulo = document.getElementById("titulo")
+var parrafo = document.getElementsByClassName("parrafo")
+var cuerpo = document.getElementById("cuerpo")
 
+
+
+titulo.innerText="Nuevo titulo"
+parrafo[0].innerText = "Parrafo 1 modificado"
+parrafo[1].innerText = "Parrafo 2 modificado"
+parrafo[2].innerText = "Parrafo 3 modificado"
+
+parrafo[0].outerHTML= "<div>Parrafo 11 mopdificado</div>"
+
+cuerpo.style.backgroundColor="#f4f4f4"
+cuerpo.style.textAlign="center"
+
+titulo.style.color="#333"
+titulo.style.fontStyle
 
