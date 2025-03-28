55c0c4cd94a455796d45f25d8f9b17b33965d4c7
Véro Grué <veronique.gru@educa.jcyl.com>
2025-03-28 17:43:25 +0100

    synch fork

--- 01/README.md
+++ 01/README.md
@@ -13,6 +13,3 @@
 *  Parrafo 1: color fuente  rojo #e74c3c; , color fondo: #ffe6e6, color borde: #c0392b
 *  Parrafo 2: color fuente verde #2ecc71, color de fondo: #eaffea, color borde: #27ea60
 *  Parrafo 3: color fuente azul #3498db color de fondo: #e6f2ff, color borde: #2980b9
-
-
-
--- 01/js/script.js
+++ 01/js/script.js
@@ -45,7 +45,7 @@ parrafo[1].style.margin="auto"
 parrafo[2].style.width="60%"
 parrafo[2].style.margin="auto"
 
-
+/*
 //border iquierdo 5px solido, border radio
 parrafo[0].style.borderLeft="solid 5px"
 parrafo[0].style.borderRadius="5px"
@@ -56,14 +56,34 @@ parrafo[2].style.borderRadius="5px"
 
 /*Parrafo 1: color fuente  rojo #e74c3c; , color fondo: #ffe6e6, 
 color borde: #c0392b */
+/*
 parrafo[0].style.color="#e74c3c"
 parrafo[0].style.backgroundColor="#ffe6e6"
 parrafo[0].style.borderColor="#c0392b"
 /**  Parrafo 2: color fuente verde #2ecc71, color de fondo: #eaffea, color borde: #27ea60*/
+/*
 parrafo[1].style.color="#2ecc71"
 parrafo[1].style.backgroundColor="#eaffea"
 parrafo[1].style.borderColor="#27ea60"
 /**  Parrafo 3: color fuente azul #3498db color de fondo: #e6f2ff, color borde: #2980b9*/
+/*
 parrafo[2].style.color="#3498db"
 parrafo[2].style.backgroundColor="e6f2ff"
 parrafo[2].style.borderColor="#2980b9"
+*/
+parrafo.forEach(p => {
+    p.style.backgroundColor="green"
+});
+/*
+for (let i = 0; i < parrafo.length; i++) {
+    parrafo[i].style.backgroundColor="red"
+    
+}*/
+
+for (let p of parrafo) {
+    p.style
+} 
+
+for await (const element of object) {
+    
+}
