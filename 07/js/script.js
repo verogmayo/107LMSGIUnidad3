95f56837539cdb705a40cfe564fad187e4f67005
Véro Grué <veronique.gru@educa.jcyl.com>
2025-04-04 07:19:23 +0200

    Ejercicio 7

--- 07/js/script.js
+++ 07/js/script.js
@@ -1,41 +1,43 @@
+var contenedor=document.getElementById("contenedor")
+
 document.getElementById("agregarFinal").addEventListener("click", function(){
     // Crear un nuevo párrafo
-   
-    //añadir texto al párrafo
-  
+   let parrafo=document.createElement("p");
+      //añadir texto al párrafo
+    parrafo.innerHTML=document.getElementById("texto").value
     //añadir el párrafo al contenedor
-   
+   contenedor.append(parrafo)
     //agregar el color de texto del nuevo parrafo sea rojo
     parrafo.style.color = "red";
 });
 //agregar párrafo al principio
 document.getElementById("agregarPpio").addEventListener("click", function(){
     // Crear un nuevo párrafo
-   
+    let parrafo=document.createElement("p");
     //añadir texto al párrafo
-   
+    parrafo.innerHTML=document.getElementById("texto").value
     //añadir el párrafo al contenedor .prepend(parrafo)
-   
+    contenedor.prepend(parrafo)
     //agregar estilos al párrafo texto azul
-   
+    parrafo.style.color = "blue";
 });
 //agregar párrafo en una posición concreta
 document.getElementById("agregarPosicion").addEventListener("click", function(){
     // Crear un nuevo párrafo
-  
+    let parrafo=document.createElement("p");
     //añadir texto al párrafo
-  
+    parrafo.innerHTML=document.getElementById("texto").value
     //añadir el párrafo al contenedor inserBefore
-  
+    contenedor.insertBefore(parrafo, contenedor.children[2]);
     //agregar estilos color del texto verde
-    
+    parrafo.style.color = "green";
 });
 //eliminar el primer párrafo
 document.getElementById("eliminar").addEventListener("click", function(){
     //eliminar el primer párrafo
-   
+   contenedor.firstChild.remove();
 });
 document.getElementById("eliminarTodos").addEventListener("click", function(){
     //eliminar todos los párrafos
-   
+    contenedor.remove();
 });
